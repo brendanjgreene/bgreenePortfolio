bc403bc88056e2f87b5464faa2f3a709a2d4704f
Brendan Greene <brendanjgreene@gmail.com>
2017-09-01 15:18:36 +0100

    my scripts

---
+++ js/onscroll.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("target").classList.add("hsmall");
+    } else {
+        document.getElementById("target").classList.remove("hsmall");
+    } if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("backToTop").style.display = "block";
+    } else {
+        document.getElementById("backToTop").style.display = "none";
+    }
+}
